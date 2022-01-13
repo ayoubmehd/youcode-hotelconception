555e00fe23ca44318dfeb436c455fde4ff6fee53
ayoubmehd <safi-_-gio-raja@live.fr>
2022-01-13 12:52:23 +0100

    feat: crud customer

---
+++ booking App/controllers/customerController.js
@@ -0,0 +1,68 @@
+const Customer = require("../models/Customer");
+
+module.exports = {
+    async getAll(req, res) {
+        const page = req.query.page ?? req.query.page <= 0 ? req.query.page : 1;
+        const perpage = 10;
+
+        const customers = await Customer.find().skip((page - 1) * perpage).limit(perpage);
+        res.json({
+            data: customers,
+            prevPage: page > 1 ? page - 1 : null,
+            currentPage: +page,
+            nextPage: page < customers.length - 1 ? +page + 1 : null
+        });
+
+    },
+    async create(req, res) {
+
+        const customer = new Customer(req.body);
+
+        await customer.save();
+
+        res.status(201).json({
+            message: "Customer created successfully",
+            data: customer,
+        });
+    },
+    async update(req, res) {
+        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body);
+
+        if (!customer) {
+            return res.status(404).json({
+                message: "Customer not found",
+            });
+        }
+
+        res.json({
+            message: "Customer updated successfully",
+            data: customer
+        });
+    },
+    async getById(req, res) {
+        const customer = await Customer.findById(req.params.id);
+
+        if (!customer) {
+            res.status(404).json({
+                message: "Customer Not Found",
+            });
+        }
+
+        res.json(customer);
+    },
+    async destroy(req, res) {
+
+        const customer = await Customer.findByIdAndDelete(req.params.id);
+
+        if (!customer) {
+            res.status(404).json({
+                message: "Customer Not Found",
+            });
+        }
+
+        res.status(202).json({
+            message: "Customer deleted sccessfully",
+            data: customer
+        });
+    }
+}
--- booking App/routers/v1/index.js
+++ booking App/routers/v1/index.js
@@ -4,9 +4,14 @@ const router = express.Router()
 
 
 const User = require('./User');
+const customer = require("./customer");
 
 
 router.use("/", User);
+
+
+
+router.use("/customers", customer);
 
 module.exports = router
 
